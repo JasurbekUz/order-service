1d510cc6ba7ba5646fd523ca9503dc9d803f298b
JasurbekUz <jasurbekedu@gmail.com>
2022-01-08 23:01:22 +0500

    submodule file added and edited some files

--- Makefile
+++ Makefile
@@ -1,15 +1,18 @@
 .PHONY: build
 
 CURRENT_DIR=$(shell pwd)
-APP=template
+APP=catalog
 APP_CMD_DIR=./cmd
 
 build:
-	CGO_ENABLED=0 GOOS=linux go build -mod=vendor -a -installsuffix cgo -o ${CURRENT_DIR}/bin/${APP} ${APP_CMD_DIR}/main.go
+        CGO_ENABLED=0 GOOS=linux go build -mod=vendor -a -installsuffix cgo -o \
+${CURRENT_DIR}/bin/${APP} ${APP_CMD_DIR}/main.go
 
 proto-gen:
-	./scripts/gen-proto.sh	${CURRENT_DIR}
-	ls genproto/*.pb.go | xargs -n1 -IX bash -c "sed -e '/bool/ s/,omitempty//' X > X.tmp && mv X{.tmp,}"
+    ./scripts/gen-proto.sh  ${CURRENT_DIR}
 
-lint: ## Run golangci-lint with printing to stdout
-	golangci-lint -c .golangci.yaml run --build-tags "musl" ./...
+lint: ## Run golangci-lint with printing to stdout                              
+        golangci-lint -c .golangci.yaml run ./...
+
+pull-submodule:
+         git submodule update --init --recursive --remote
--- scripts/gen-proto.sh
+++ scripts/gen-proto.sh
@@ -4,16 +4,27 @@ CURRENT_DIR=$(pwd)
 
 protoc -I /usr/local/include \
        -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
-       -I $CURRENT_DIR/protos/ \
-        --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/ \
-        $CURRENT_DIR/protos/*.proto;
+       -I $CURRENT_DIR/book-shop_protos/catalog_service/ \
+        --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/catalog_service/ \
+        $CURRENT_DIR/book-shop_protos/catalog_service/*.proto;
+
+protoc -I /usr/local/include \
+       -I $GOPATH/src/github.com/gogo/protobuf/gogoproto \
+       -I $CURRENT_DIR/book-shop_protos/order_service/ \
+        --gofast_out=plugins=grpc:$CURRENT_DIR/genproto/order_service/ \
+        $CURRENT_DIR/book-shop_protos/order_service/*.proto;        
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/*.go
+    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/catalog_service/*.go
   else
-    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/*.go
+    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/catalog_service/*.go
 fi
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i "" -e "s/,omitempty//g" $CURRENT_DIR/genproto/order_service/*.go
+  else
+    sed -i -e "s/,omitempty//g" $CURRENT_DIR/genproto/order_service/*.go
+fi
 # make proto-gen 
 # cd scripts 
 # ll // -rw-rw-r-- => for + x
